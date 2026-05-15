4bf057aa09f91d283ee6002dbc16ca96bfe855fa
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-15 14:26:37 +0900

    004

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -5,36 +5,44 @@ import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
-    @GetMapping("/posts/write")
-    @ResponseBody
-    public String write() {
-        return """
-                <form action="dowrite">
-                  <input type="text" name="title" placeholder="제목" value="안녕">
+
+    private String getWriteFormHtml() {
+        return getWriteFormHtml("");    }
+
+    private String getWriteFormHtml(String errorMessage) {
+       return """
+                <div style="color: red;">%s</div>
+                <form method="POST" action="doWrite">
+                <input type="text" name="title" placeholder="제목" value="">
                   <br>
-                  <textarea name="content" placeholder="내용">반가워.</textarea>
+                  <textarea name="content" placeholder="내용"></textarea>
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """;
+                """.formatted(errorMessage);
     }
-    @GetMapping("/posts/dowrite")
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+        return getWriteFormHtml();
+    }
+    @PostMapping ("/posts/doWrite")
     @ResponseBody
     @Transactional
     public String write (
         @RequestParam(defaultValue = "") String title,
         @RequestParam(defaultValue = "") String content
     ){
-       if (title.isBlank()) return "제목을 입력해주세요.";
-       if (content.isBlank()) return "내용을 입력해주세요.";
+       if (title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.");
+       if (content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.");
+
 
        Post post =  postService.write(title, content);
 
