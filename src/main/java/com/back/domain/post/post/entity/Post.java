ea0193caa4321a2a5a71928fe09641814457f9d2
hiysk0610-hi <hiysk0610@gmail.com>
2026-05-18 21:51:36 +0900

    024

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,9 +1,17 @@
 package com.back.domain.post.post.entity;
 
+import com.back.domain.post.postComent.entity.PostComment;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.*;
+import static jakarta.persistence.CascadeType.PERSIST;
 
 @Entity
 @Getter
@@ -11,6 +19,8 @@ import lombok.NoArgsConstructor;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE})
+    private List<PostComment> comments = new ArrayList<>();
 
     public Post(String title, String content) {
         this.title = title;
@@ -20,4 +30,13 @@ public class Post extends BaseEntity {
         this.title = title;
         this.content = content;
     }
+
+    public PostComment addComment(String content) {
+        PostComment postComment = new PostComment(this, content);
+        comments.add(postComment);
+
+        return postComment;
+
+       }
+
 }
